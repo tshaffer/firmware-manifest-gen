b80b60173e153e37367c064bd128e142ebbe8c33
Ted Shaffer <ted@brightsign.biz>
2018-01-07 12:20:53 -0800

    remove redux

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,21 +1,6 @@
 import * as React from 'react';
 
-// import { connect } from 'react-redux';
-// import { bindActionCreators, Dispatch } from 'redux';
-//
-// import {
-//     ArState,
-// } from '../types';
-
 export default class App extends React.Component<any, object> {
-
-    state: object;
-
-    // constructor(props: object) {
-    //     super(props);
-    //
-    //     myApp = this;
-    // }
 
     render() {
       return (
@@ -23,12 +8,3 @@ export default class App extends React.Component<any, object> {
       );
     }
 }
-
-// function mapStateToProps(state : ArState) {
-//
-//     return {
-//         bsdm: state.bsdm,
-//     };
-// }
-//
-// export default connect(mapStateToProps, null)(App);
