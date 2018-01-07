5f8c675c3367c26a94aa7bf58eeaf74a34176f9c
Ted Shaffer <ted@brightsign.biz>
2018-01-07 12:14:09 -0800

    initial checkin

---
+++ src/components/app.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+
+// import { connect } from 'react-redux';
+// import { bindActionCreators, Dispatch } from 'redux';
+//
+// import {
+//     ArState,
+// } from '../types';
+
+export default class App extends React.Component<any, object> {
+
+    state: object;
+
+    // constructor(props: object) {
+    //     super(props);
+    //
+    //     myApp = this;
+    // }
+
+    render() {
+      return (
+        <div>Pizza</div>
+      );
+    }
+}
+
+// function mapStateToProps(state : ArState) {
+//
+//     return {
+//         bsdm: state.bsdm,
+//     };
+// }
+//
+// export default connect(mapStateToProps, null)(App);
