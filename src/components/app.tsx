43be9d62632c98229c849b967d2078455d8dbe53
Ted Shaffer <ted@brightsign.biz>
2018-01-21 11:25:57 -0800

    moved httpUploadFile to utilities

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -9,11 +9,6 @@ import * as React from 'react';
 import * as path from 'path';
 import isomorphicPath from 'isomorphic-path';
 
-import * as fse from 'fs-extra';
-import * as isomorphicFetch from 'isomorphic-fetch';
-
-import * as FormData from 'form-data';
-
 import * as recursive from 'recursive-readdir';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -29,7 +24,8 @@ import {
 } from '../interfaces';
 
 import {
-  getFileInfo
+  getFileInfo,
+  httpUploadFile,
 } from '../utilities';
 
 export default class App extends React.Component<any, object> {
@@ -49,6 +45,18 @@ export default class App extends React.Component<any, object> {
     this.handleContentFolderChange = this.handleContentFolderChange.bind(this);
     this.handleBrightSignIpAddressChange = this.handleBrightSignIpAddressChange.bind(this);
     this.handleBeginTransfer = this.handleBeginTransfer.bind(this);
+  }
+
+  setStatus(status: string) {
+    this.setState({
+      status,
+    });
+  }
+
+  appendStatus(statusToAppend: string) {
+    let xferStatus: string = this.state.status;
+    xferStatus += '\n' + statusToAppend;
+    this.setStatus(xferStatus);
   }
 
   handleBrowse = () => {
@@ -89,8 +97,6 @@ export default class App extends React.Component<any, object> {
 
         // TODO - check err
         // `files` is an array of absolute file paths
-        // console.log(err);
-        // console.log(files);
 
         files.forEach( (fullPath) => {
           promises.push(getFileInfo(fullPath));
@@ -153,42 +159,11 @@ export default class App extends React.Component<any, object> {
     });
   }
 
-  httpUploadFile(hostname: string,
-                 endpoint: string,
-                 filePath: string,
-                 uploadHeaders: any[] = []): Promise<any> {
-    return new Promise((resolve, reject) => {
-      const url = 'http://' + hostname + ':8080' + endpoint; // TODO this should be a global const
-
-      // TODO get headers should be a utility function
-      const headers: any = {};
-      uploadHeaders.forEach((uploadHeader) => {
-        headers[uploadHeader.key] = uploadHeader.value;
-      });
-
-      // TODO get formData should be a utility function
-      const formData = new FormData();
-      formData.append('files', fse.createReadStream(filePath));
-      isomorphicFetch(url, {
-        method: 'POST',
-        headers,
-        body: formData
-      }).then((response: any) => {
-// TODO this should return response.json(). However, there are currently clients of this function
-// which make device api calls that respond with empty or non JSON bodies. These calls should be
-// handled through a different function.
-        resolve(response);
-      }).catch((err: any) => {
-        reject(err);
-      });
-    });
-  }
-
   getFilesToTransfer(ipAddress: string, filePath: string) : Promise<FileToTransferBs[]> {
 
     return new Promise((resolve, reject) => {
 
-      this.httpUploadFile(ipAddress, '/GetFilesToTransfer', filePath, []).then((response: any) => {
+      httpUploadFile(ipAddress, '/GetFilesToTransfer', filePath, []).then((response: any) => {
         return response.json();
       }).then((filesToTransfer: FileToTransferBs[]) => {
         resolve(filesToTransfer);
@@ -206,7 +181,7 @@ export default class App extends React.Component<any, object> {
     header.value = destinationRelativePath;
     headers.push(header);
 
-    return this.httpUploadFile(ipAddress, endPoint, sourcePath, headers);
+    return httpUploadFile(ipAddress, endPoint, sourcePath, headers);
   }
 
   transferFiles(siteFolder: string, filesToTransfer: FileToTransferBs[], ipAddress: string) {
@@ -217,18 +192,6 @@ export default class App extends React.Component<any, object> {
       const fullPath = isomorphicPath.join(siteFolder, relativePathToTransfer);
       const promise: any = this.uploadFileToBrightSign(fullPath, relativePathToTransfer, ipAddress);
     });
-  }
-
-  setStatus(status: string) {
-    this.setState({
-      status,
-    });
-  }
-
-  appendStatus(statusToAppend: string) {
-    let xferStatus: string = this.state.status;
-    xferStatus += '\n' + statusToAppend;
-    this.setStatus(xferStatus);
   }
 
   handleBeginTransfer() {
--- src/utilities.ts
+++ src/utilities.ts
@@ -1,10 +1,47 @@
-import * as fs from 'fs-extra';
+import * as fse from 'fs-extra';
+
 import * as crypto from 'crypto';
 
 import * as string_decoder from 'string_decoder';
 const decoder = new string_decoder.StringDecoder('utf8');
 
+import * as FormData from 'form-data';
+
+import * as isomorphicFetch from 'isomorphic-fetch';
+
 import { FileInfo } from './interfaces';
+
+export function httpUploadFile(hostname: string,
+                               endpoint: string,
+                               filePath: string,
+                               uploadHeaders: any[] = []): Promise<any> {
+
+  return new Promise((resolve, reject) => {
+    const url = 'http://' + hostname + ':8080' + endpoint; // TODO this should be a global const
+
+    // TODO get headers should be a utility function
+    const headers: any = {};
+    uploadHeaders.forEach((uploadHeader) => {
+      headers[uploadHeader.key] = uploadHeader.value;
+    });
+
+    // TODO get formData should be a utility function
+    const formData = new FormData();
+    formData.append('files', fse.createReadStream(filePath));
+    isomorphicFetch(url, {
+      method: 'POST',
+      headers,
+      body: formData
+    }).then((response: any) => {
+// TODO this should return response.json(). However, there are currently clients of this function
+// which make device api calls that respond with empty or non JSON bodies. These calls should be
+// handled through a different function.
+      resolve(response);
+    }).catch((err: any) => {
+      reject(err);
+    });
+  });
+}
 
 export function getFileInfo(filePath: string): Promise<FileInfo> {
 
@@ -12,7 +49,7 @@ export function getFileInfo(filePath: string): Promise<FileInfo> {
 
   return new Promise((resolve) => {
     const hash = crypto.createHash('sha1');
-    const input = fs.createReadStream(filePath);
+    const input = fse.createReadStream(filePath);
     input.on('readable', () => {
       const data = input.read();
       if (data) {
